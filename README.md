b7354b4633e6850badab5b2cb5f071655dbcb99d
Stephen Wachanga <wachangasteve@gmail.com>
2021-07-01 06:42:30 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Network Billing System
+
+A complete biling system integrated with pfsense.
+
+#### License
+
+MIT
---
+++ network_billing_system/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Network Billing System",
+			"color": "yellow",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Network Billing System")
+		}
+	]
