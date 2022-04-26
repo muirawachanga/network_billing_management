6536ccb5fa4bc79cf21740def9e47d0f680dc882
Stephen Wachanga <wachangasteve@gmail.com>
2022-04-26 22:32:40 +0000

    Resolve not found doctype; Kopokopo setting for the first installation.

--- network_billing_system/utils.py
+++ network_billing_system/utils.py
@@ -1,7 +1,11 @@
 import frappe
 
-
 def load_configuration(name, default=None):
+    # check if the app is installed or not, otherwise an error is exempted
+    # when Kopokopo doctype is not installed.
+    installed_apps = frappe.get_installed_apps()
+    if "network_billing_system" not in installed_apps:
+        return
     val = frappe.db.get_single_value("Kopokopo Mpesa Setting", name)
     if val is None:
         val = default
