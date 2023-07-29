2810f8e471905088ce79fbdcb4ab71bbe11fd64f
muirawachanga <30588608+muirawachanga@users.noreply.github.com>
2023-07-29 18:45:41 +0300

    Update hooks.py
    prevent double call

--- network_billing_system/hooks.py
+++ network_billing_system/hooks.py
@@ -105,11 +105,11 @@ app_license = "MIT"
 # 		"on_trash": "method"
 # 	}
 # }
-doc_events = {
-    "SMS Logs": {
-        "after_insert": "network_billing_system.network_billing_system.doctype.sms_logs.sms_logs.resend_sms"
-    }
-}
+#doc_events = {
+#    "SMS Logs": {
+#        "after_insert": "network_billing_system.network_billing_system.doctype.sms_logs.sms_logs.resend_sms"
+#   }
+#}
 
 # Scheduled Tasks
 # ---------------
